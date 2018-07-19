628d08a123fa6a3a036053d47e14a4401c5df26a
Vanesa Reinerth <vanesa@getbowtied.com>
2018-07-19 15:11:27 +0300

    plugin info & file cleanup

--- shopkeeper-extender.php
+++ shopkeeper-extender.php
@@ -1,17 +1,17 @@
 <?php
+
 /**
- * Plugin Name: GetBowtied Custom Gutenberg Blocks
- * Plugin URI: http://getbowtied.com
- * Description: Gutenberg, a new editing experience for WordPress is in the works and this is a beta plugin to add a social sharing block within that upcoming editor.
- * Author: GetBowtied
- * Version: 1.0.0
- * Text Domain: getbowtied
- * Domain Path: languages
- * Requires at least: 4.7
- * Tested up to: 4.9.1
+ * Plugin Name:       		Shopkeeper Extender
+ * Plugin URI:        		https://github.com/getbowtied/Hook-Me-Up
+ * Description:       		Extends the functionality of Shopkeeper with Gutenberg elements.
+ * Version:           		1.0
+ * Author:            		GetBowtied
+ * Author URI:        		https://getbowtied.com
+ * Requires at least: 		4.9
+ * Tested up to: 			4.9.7
  *
- * @package   getbowtied
- * @author    GetBowtied
+ * @package  Shopkeeper Extender
+ * @author   GetBowtied
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -34,7 +34,7 @@ function gbt_gutenberg_blocks() {
 	}
 
 	if( is_plugin_active( 'gutenberg/gutenberg.php' ) || is_wp_version('>=', '5.0') ) {
-		require_once 'blocks/index.php';
+		require_once 'includes/gbt-blocks/index.php';
 	} else {
 		add_action( 'admin_notices', 'theme_warning' );
 	}
@@ -44,7 +44,7 @@ add_action( 'init', 'gbt_gutenberg_blocks' );
 function theme_warning() {
 
 	echo '<div class="message error woocommerce-admin-notice woocommerce-st-inactive woocommerce-not-configured">';
-	echo '<p>' . esc_html( 'GetBowtied Custom Gutenberg Blocks is enabled but not effective. Please activate Gutenberg plugin in order to work.', 'gbt-blocks' ) . '</p>';
+	echo '<p>GetBowtied Custom Gutenberg Blocks is enabled but not effective. Please activate Gutenberg plugin in order to work.</p>';
 	echo '</div>';
 }
 
