fdc0c457afd608ed44c8471f136e3b823a85d4fa
Vanesa Reinerth <vanesa@getbowtied.com>
2018-07-20 14:23:09 +0300

    plugin updater

---
+++ README.txt
@@ -0,0 +1,27 @@
+=== Shopkeeper Extender ===
+Contributors: getbowtied, vanesareinerth, adrianlbs
+Tags: gutenberg, blocks
+Requires at least: 4.9
+Tested up to: 4.9.7
+Stable tag: 1.0
+Requires PHP: 5.5.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Extends the functionality of Shopkeeper with Gutenberg elements.
+
+== Description ==
+
+Extends the functionality of Shopkeeper with Gutenberg elements.
+
+== Installation ==
+
+The easiest way:
+* In your WordPress admin dashboard navigate to the **Plugins** > **Add New**.
+* Search for “HookMeUp”
+* Click the “Install” button.
+
+== Changelog ==
+
+= 1.0 =
+- Initial Version
--- shopkeeper-extender.php
+++ shopkeeper-extender.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name:       		Shopkeeper Extender
- * Plugin URI:        		https://github.com/getbowtied/Hook-Me-Up
+ * Plugin URI:        		https://github.com/getbowtied/shopkeeper-extender
  * Description:       		Extends the functionality of Shopkeeper with Gutenberg elements.
  * Version:           		1.0
  * Author:            		GetBowtied
@@ -22,9 +22,33 @@ if ( ! function_exists( 'is_plugin_active' ) ) {
     require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 }
 
-/**
- * Load getbowtied blocks.
- */
+add_action( 'init', 'github_plugin_updater' );
+function github_plugin_updater() {
+
+	include_once 'updater.php';
+
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	if ( is_admin() ) {
+
+		$config = array(
+			'slug' 				 => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'shopkeeper-extender',
+			'api_url' 			 => 'https://api.github.com/repos/getbowtied/shopkeeper-extender',
+			'raw_url' 			 => 'https://raw.github.com/getbowtied/shopkeeper-extender/master',
+			'github_url' 		 => 'https://github.com/getbowtied/shopkeeper-extender',
+			'zip_url' 			 => 'https://github.com/getbowtied/shopkeeper-extender/zipball/master',
+			'sslverify'			 => true,
+			'requires'			 => '4.9',
+			'tested'			 => '4.9.7',
+			'readme'			 => 'README.txt',
+			'access_token'		 => '',
+		);
+
+		new WP_GitHub_Updater( $config );
+
+	}
+}
 
 function gbt_gutenberg_blocks() {
 
@@ -34,7 +58,7 @@ function gbt_gutenberg_blocks() {
 	}
 
 	if( is_plugin_active( 'gutenberg/gutenberg.php' ) || is_wp_version('>=', '5.0') ) {
-		require_once 'includes/gbt-blocks/index.php';
+		include_once 'includes/gbt-blocks/index.php';
 	} else {
 		add_action( 'admin_notices', 'theme_warning' );
 	}
