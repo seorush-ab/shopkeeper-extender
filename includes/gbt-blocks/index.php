2da8fa5d60e65f6d7196511251535bd9803570ad
Vanesa Reinerth <vanesa@getbowtied.com>
2018-11-13 10:55:04 +0200

    block settings

--- includes/gbt-blocks/index.php
+++ includes/gbt-blocks/index.php
@@ -1,26 +1,33 @@
 <?php
 
-// Shopkeeper Custom Gutenberg Blocks
- 
-add_filter( 'block_categories', function( $categories, $post ) {
+//==============================================================================
+//	Main Editor Styles
+//==============================================================================
+wp_enqueue_style(
+	'getbowtied-product-blocks-editor-styles',
+	plugins_url( 'assets/css/editor.css', __FILE__ ),
+	array( 'wp-edit-blocks' )
+);
 
-	if ( $post &&  $post->post_type !== 'post' && $post->post_type !== 'page' && $post->post_type !== 'portfolio' ) {
-		return $categories;
+//==============================================================================
+//	Main JS
+//==============================================================================
+add_action( 'admin_init', 'getbowtied_product_blocks_scripts' );
+if ( ! function_exists( 'getbowtied_product_blocks_scripts' ) ) {
+	function getbowtied_product_blocks_scripts() {
+
+		wp_enqueue_script(
+			'getbowtied-product-blocks-editor-scripts',
+			plugins_url( 'assets/js/main.js', __FILE__ ),
+			array( 'wp-blocks', 'jquery' )
+		);
+
 	}
-	return array_merge(
-		array(
-			array(
-				'slug' => 'shopkeeper',
-				'title' => __( 'Shopkeeper', 'gbt-blocks' ),
-			),
-		),
-		$categories
-	);
-}, 10, 2 );
+}
 
-require_once 'categories_grid/index.php';
-require_once 'latest_posts_grid/index.php';
-require_once 'banner/index.php';
-require_once 'portfolio/index.php';
-require_once 'social-media-profiles/index.php';
-require_once 'slider/index.php';
+require_once 'categories_grid/block.php';
+// require_once 'latest_posts_grid/index.php';
+require_once 'banner/block.php';
+// require_once 'portfolio/index.php';
+// require_once 'social-media-profiles/index.php';
+// require_once 'slider/index.php';
