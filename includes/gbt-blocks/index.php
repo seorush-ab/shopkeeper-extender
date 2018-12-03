f404785d4b490d097d04c43df919bce97d0e38ca
Vanesa Reinerth <vanesa@getbowtied.com>
2018-12-03 13:29:09 +0200

    ...

--- includes/gbt-blocks/index.php
+++ includes/gbt-blocks/index.php
@@ -4,7 +4,7 @@
 //	Main Editor Styles
 //==============================================================================
 wp_enqueue_style(
-	'getbowtied-product-blocks-editor-styles',
+	'getbowtied-sk-product-blocks-editor-styles',
 	plugins_url( 'assets/css/editor.css', __FILE__ ),
 	array( 'wp-edit-blocks' )
 );
@@ -12,12 +12,12 @@ wp_enqueue_style(
 //==============================================================================
 //	Main JS
 //==============================================================================
-add_action( 'admin_init', 'getbowtied_product_blocks_scripts' );
-if ( ! function_exists( 'getbowtied_product_blocks_scripts' ) ) {
-	function getbowtied_product_blocks_scripts() {
+add_action( 'admin_init', 'getbowtied_sk_product_blocks_scripts' );
+if ( ! function_exists( 'getbowtied_sk_product_blocks_scripts' ) ) {
+	function getbowtied_sk_product_blocks_scripts() {
 
 		wp_enqueue_script(
-			'getbowtied-product-blocks-editor-scripts',
+			'getbowtied-sk-product-blocks-editor-scripts',
 			plugins_url( 'assets/js/main.js', __FILE__ ),
 			array( 'wp-blocks', 'jquery' )
 		);
