4770bc82444fc1f03772d0e14adc27fbce2f4f2f
Vanesa Reinerth <vanesa@getbowtied.com>
2018-12-07 14:53:28 +0200

    portfolio changed block icon

--- includes/gbt-blocks/portfolio/block.js
+++ includes/gbt-blocks/portfolio/block.js
@@ -20,7 +20,10 @@
 	/* Register Block */
 	registerBlockType( 'getbowtied/sk-portfolio', {
 		title: i18n.__( 'Portfolio' ),
-		icon: 'format-gallery',
+		icon:
+			el( SVG, { xmlns:'http://www.w3.org/2000/svg', viewBox:'0 0 24 24' },
+				el( Path, { d:'M14 6V4h-4v2h4zM4 8v11h16V8H4zm16-2c1.11 0 2 .89 2 2v11c0 1.11-.89 2-2 2H4c-1.11 0-2-.89-2-2l.01-11c0-1.11.88-2 1.99-2h4V4c0-1.11.89-2 2-2h4c1.11 0 2 .89 2 2v2h4z' } ),
+			),
 		category: 'shopkeeper',
 		supports: {
 			align: [ 'center', 'wide', 'full' ],
