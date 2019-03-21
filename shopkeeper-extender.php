b83e6fda83e8450863ad1c827c770a3977c42f08
Vanesa Reinerth <vanesa@getbowtied.com>
2019-03-21 16:55:52 +0200

    deploy test

--- shopkeeper-extender.php
+++ shopkeeper-extender.php
@@ -4,7 +4,7 @@
  * Plugin Name:       		Shopkeeper Extender
  * Plugin URI:        		https://shopkeeper.wp-theme.design/
  * Description:       		Extends the functionality of Shopkeeper with Gutenberg elements.
- * Version:           		1.3.4
+ * Version:           		1.3.5
  * Author:            		GetBowtied
  * Author URI:				https://getbowtied.com
  * Text Domain:				shopkeeper-extender
