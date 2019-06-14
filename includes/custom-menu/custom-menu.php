f8c27f588dfbca16fb21881b2be71b24341b2250
Vanesa <vanesa.reinerth@gmail.com>
2019-06-14 12:36:54 +0300

    fix for custom menu error

--- includes/custom-menu/custom-menu.php
+++ includes/custom-menu/custom-menu.php
@@ -11,91 +11,94 @@ Text Domain: rc_scm
 Domain Path: languages
 */
 
-class rc_sweet_custom_menu {
+if( !class_exists('rc_sweet_custom_menu')) {
+	
+	class rc_sweet_custom_menu {
 
-	/*--------------------------------------------*
-	 * Constructor
-	 *--------------------------------------------*/
+		/*--------------------------------------------*
+		 * Constructor
+		 *--------------------------------------------*/
 
-	/**
-	 * Initializes the plugin by setting localization, filters, and administration functions.
-	 */
-	function __construct() {
+		/**
+		 * Initializes the plugin by setting localization, filters, and administration functions.
+		 */
+		function __construct() {
 
-		// load the plugin translation files
-		add_action( 'init', array( $this, 'textdomain' ) );
+			// load the plugin translation files
+			add_action( 'init', array( $this, 'textdomain' ) );
+			
+			// add custom menu fields to menu
+			add_filter( 'wp_setup_nav_menu_item', array( $this, 'rc_scm_add_custom_nav_fields' ) );
+
+			// save menu custom fields
+			add_action( 'wp_update_nav_menu_item', array( $this, 'rc_scm_update_custom_nav_fields'), 10, 3 );
+			
+			// edit menu walker
+			add_filter( 'wp_edit_nav_menu_walker', array( $this, 'rc_scm_edit_walker'), 10, 2 );
+
+		} // end constructor
 		
-		// add custom menu fields to menu
-		add_filter( 'wp_setup_nav_menu_item', array( $this, 'rc_scm_add_custom_nav_fields' ) );
-
-		// save menu custom fields
-		add_action( 'wp_update_nav_menu_item', array( $this, 'rc_scm_update_custom_nav_fields'), 10, 3 );
 		
-		// edit menu walker
-		add_filter( 'wp_edit_nav_menu_walker', array( $this, 'rc_scm_edit_walker'), 10, 2 );
+		/**
+		 * Load the plugin's text domain
+		 *
+		 * @since 1.0
+		 *
+		 * @return void
+		 */
+		public function textdomain() {
+			load_plugin_textdomain( 'rc_scm', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+		}
+		
+		/**
+		 * Add custom fields to $item nav object
+		 * in order to be used in custom Walker
+		 *
+		 * @access      public
+		 * @since       1.0 
+		 * @return      void
+		*/
+		function rc_scm_add_custom_nav_fields( $menu_item ) {
+		
+		    $menu_item->background_url = get_post_meta( $menu_item->ID, '_menu_item_background_url', true );
+		    return $menu_item;
+		    
+		}
+		
+		/**
+		 * Save menu custom fields
+		 *
+		 * @access      public
+		 * @since       1.0 
+		 * @return      void
+		*/
+		function rc_scm_update_custom_nav_fields( $menu_id, $menu_item_db_id, $args ) {
+		
+		    // Check if element is properly sent
+		    if ( !empty( $_REQUEST['menu-item-background_url']) ) {
 
-	} // end constructor
-	
-	
-	/**
-	 * Load the plugin's text domain
-	 *
-	 * @since 1.0
-	 *
-	 * @return void
-	 */
-	public function textdomain() {
-		load_plugin_textdomain( 'rc_scm', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-	}
-	
-	/**
-	 * Add custom fields to $item nav object
-	 * in order to be used in custom Walker
-	 *
-	 * @access      public
-	 * @since       1.0 
-	 * @return      void
-	*/
-	function rc_scm_add_custom_nav_fields( $menu_item ) {
-	
-	    $menu_item->background_url = get_post_meta( $menu_item->ID, '_menu_item_background_url', true );
-	    return $menu_item;
-	    
-	}
-	
-	/**
-	 * Save menu custom fields
-	 *
-	 * @access      public
-	 * @since       1.0 
-	 * @return      void
-	*/
-	function rc_scm_update_custom_nav_fields( $menu_id, $menu_item_db_id, $args ) {
-	
-	    // Check if element is properly sent
-	    if ( !empty( $_REQUEST['menu-item-background_url']) ) {
-
-	    	if (isset($_REQUEST['menu-item-background_url'][$menu_item_db_id])) {
-		        $background_url_value = $_REQUEST['menu-item-background_url'][$menu_item_db_id];
-		        update_post_meta( $menu_item_db_id, '_menu_item_background_url', $background_url_value );
+		    	if (isset($_REQUEST['menu-item-background_url'][$menu_item_db_id])) {
+			        $background_url_value = $_REQUEST['menu-item-background_url'][$menu_item_db_id];
+			        update_post_meta( $menu_item_db_id, '_menu_item_background_url', $background_url_value );
+			    }
 		    }
-	    }
-	    
-	}
-	
-	/**
-	 * Define new Walker edit
-	 *
-	 * @access      public
-	 * @since       1.0 
-	 * @return      void
-	*/
-	function rc_scm_edit_walker($walker,$menu_id) {
-	
-	    return 'Walker_Nav_Menu_Edit_Custom';
-	    
-	}
+		    
+		}
+		
+		/**
+		 * Define new Walker edit
+		 *
+		 * @access      public
+		 * @since       1.0 
+		 * @return      void
+		*/
+		function rc_scm_edit_walker($walker,$menu_id) {
+		
+		    return 'Walker_Nav_Menu_Edit_Custom';
+		    
+		}
 
+	}
 }
 
 // instantiate plugin's class
