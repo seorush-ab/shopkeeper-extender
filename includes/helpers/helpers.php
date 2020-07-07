2613f9365eb464ebfc798417fd1a449e6039efb0
Vanesa Reinerth <vanesa@getbowtied.com>
2020-07-07 12:25:48 +0300

    file management fixes

--- includes/helpers/helpers.php
+++ includes/helpers/helpers.php
@@ -1,27 +1,5 @@
 <?php
 
-if( !function_exists('theme_warning') ) {
-	function theme_warning() {
-
-		?>
-
-		<div class="error">
-			<p>Shopkeeper Extender plugin couldn't find the Block Editor (Gutenberg) on this site. It requires WordPress 5+ or Gutenberg installed as a plugin.</p>
-		</div>
-
-		<?php
-	}
-}
-
-if( !function_exists('is_wp_version') ) {
-	function is_wp_version( $operator, $version ) {
-
-		global $wp_version;
-
-		return version_compare( $wp_version, $version, $operator );
-	}
-}
-
 function sk_bool_to_string( $bool ) {
 	$bool = is_bool( $bool ) ? $bool : ( 'yes' === $bool || 1 === $bool || 'true' === $bool || '1' === $bool );
 
--- shopkeeper-extender.php
+++ shopkeeper-extender.php
@@ -57,67 +57,45 @@ if ( ! class_exists( 'ShopkeeperExtender' ) ) :
 		*/
 		public function __construct() {
 
-			$theme = wp_get_theme();
-			$parent_theme = $theme->parent();
-
 			// Helpers
 			include_once( dirname( __FILE__ ) . '/includes/helpers/helpers.php' );
 
 			// Vendor
 			include_once( dirname( __FILE__ ) . '/includes/vendor/enqueue.php' );
 
-			if( ( $theme->template == 'shopkeeper' && ( $theme->version >= '2.8' || ( !empty($parent_theme) && $parent_theme->version >= '2.8' ) ) ) || $theme->template != 'shopkeeper' ) {
+            // Customizer
+			include_once( dirname( __FILE__ ) . '/includes/customizer/repeater/class-sk-ext-repeater-control.php' );
 
-                // Customizer
-				include_once( dirname( __FILE__ ) . '/includes/customizer/repeater/class-sk-ext-repeater-control.php' );
+			// Shortcodes
+			include_once( dirname( __FILE__ ) . '/includes/shortcodes/index.php' );
 
-				// Shortcodes
-				include_once( dirname( __FILE__ ) . '/includes/shortcodes/index.php' );
+			// Social Media
+			include_once( dirname( __FILE__ ) . '/includes/social-media/class-social-media.php' );
 
-				// Social Media
-				include_once( dirname( __FILE__ ) . '/includes/social-media/class-social-media.php' );
-
-				// Widgets
-				include_once( 'includes/widgets/social-media.php' );
-
-				// Addons
-				if ( $theme->template == 'shopkeeper' && is_plugin_active( 'woocommerce/woocommerce.php') ) {
-					include_once( dirname( __FILE__ ) . '/includes/addons/class-wc-category-header-image.php' );
-				}
-			}
+			// Widgets
+			include_once( 'includes/widgets/social-media.php' );
 
 			// Gutenberg Blocks
-			add_action( 'init', array( $this, 'gbt_sk_gutenberg_blocks' ) );
+			include_once( dirname( __FILE__ ) . '/includes/gbt-blocks/index.php' );
 
-			if( $theme->template == 'shopkeeper' && ( $theme->version >= '2.8.1' || ( !empty($parent_theme) && $parent_theme->version >= '2.8.1' ) ) ) {
+            // Shopkeeper Dependent Components
+			if( function_exists('shopkeeper_theme_slug') ) {
 
-				// Custom Code Section
+				//Custom Menu
+				include_once( dirname( __FILE__ ) . '/includes/custom-menu/index.php' );
+
+                // Custom Code Section
 				include_once( dirname( __FILE__ ) . '/includes/custom-code/class-custom-code.php' );
 
 				// Social Sharing Buttons
 				if ( is_plugin_active( 'woocommerce/woocommerce.php') ) {
 					include_once( dirname( __FILE__ ) . '/includes/social-sharing/class-social-sharing.php' );
 				}
-			}
 
-			if( $theme->template == 'shopkeeper' && ( $theme->version >= '2.8.6' || ( !empty($parent_theme) && $parent_theme->version >= '2.8.6' ) ) ) {
-
-				//Custom Menu
-				include_once( dirname( __FILE__ ) . '/includes/custom-menu/index.php' );
-			}
-		}
-
-		/**
-		 * Loads Gutenberg blocks
-		 *
-		 * @return void
-		*/
-		public function gbt_sk_gutenberg_blocks() {
-
-			if( is_plugin_active( 'gutenberg/gutenberg.php' ) || is_wp_version('>=', '5.0') ) {
-				include_once( dirname( __FILE__ ) . '/includes/gbt-blocks/index.php' );
-			} else {
-				add_action( 'admin_notices', 'theme_warning' );
+                // Addons
+    			if ( is_plugin_active( 'woocommerce/woocommerce.php') ) {
+    				include_once( dirname( __FILE__ ) . '/includes/addons/class-wc-category-header-image.php' );
+    			}
 			}
 		}
 
@@ -135,4 +113,6 @@ if ( ! class_exists( 'ShopkeeperExtender' ) ) :
 	}
 endif;
 
-$shopkeeper_extender = new ShopkeeperExtender;
+add_action( 'after_setup_theme', function() {
+    $shopkeeper_extender = new ShopkeeperExtender;
+} );
