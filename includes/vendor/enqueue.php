19097b9666cac4145771f8d65973d3213c418eca
rthvanesa <vanesa.reinerth@gmail.com>
2021-04-07 15:59:20 +0300

    load swiper

--- includes/vendor/enqueue.php
+++ includes/vendor/enqueue.php
@@ -1,23 +1,19 @@
 <?php
 
-$theme = wp_get_theme();
-if ( $theme->template != 'shopkeeper') {
+add_action( 'wp_enqueue_scripts', 'getbowtied_vendor_scripts', 99 );
+function getbowtied_vendor_scripts() {
 
-	add_action( 'wp_enqueue_scripts', 'getbowtied_vendor_scripts', 99 );
-	function getbowtied_vendor_scripts() {
-
-		wp_register_style(
-			'swiper',
-			plugins_url( 'swiper/css/swiper.min.css', __FILE__ ),
-			array(),
-			'6.4.1'
-		);
-		wp_register_script(
-			'swiper',
-			plugins_url( 'swiper/js/swiper.min.js', __FILE__ ),
-			array(),
-			'6.4.1',
-			true
-		);
-	}
+	wp_register_style(
+		'swiper',
+		plugins_url( 'swiper/css/swiper.min.css', __FILE__ ),
+		array(),
+		'6.4.1'
+	);
+	wp_register_script(
+		'swiper',
+		plugins_url( 'swiper/js/swiper.min.js', __FILE__ ),
+		array(),
+		'6.4.1',
+		true
+	);
 }
