cba37bd67081ed6d7fc6d17ce01100a93cd0787c
Romi <romi@getbowtied.com>
2023-01-21 16:05:09 +0200

    1.7.2

--- includes/helpers/helpers.php
+++ includes/helpers/helpers.php
@@ -39,7 +39,38 @@ function sk_ext_is_topbar_enabled(){
 }
 
 // Other Plugins
+
 function gbt_other_plugins_enqueue_scripts() {
    wp_enqueue_script( 'gbt-other-plugins', plugins_url( 'js/other_plugins.js', __FILE__ ), array('jquery') );
 }
 add_action( 'admin_enqueue_scripts', 'gbt_other_plugins_enqueue_scripts' );
+
+
+
+// Admin notices
+
+function shopkeeper_extender_notifications() {
+
+	if ( !get_option('dismissed-sk-ext-notification-remove-additional-js', FALSE) ) {
+		?>
+		<div class="notice-error settings-error notice is-dismissible remove-additional-js-notice" style="border-width: 3px; border-color: #d63638;">
+			<p style="padding: 30px">
+				<strong style="color: #d63638">IMPORTANT NOTICE!</strong> <strong>YOUR ACTION IS REQUIRED: </strong>Please migrate your code from <em>Appearance > Customize > Additional JS</em>. This section will be removed in the next update and your code may be at risk.<br /><a href="<?php echo admin_url("customize.php?autofocus[section]=sk_custom_code"); ?>"><strong>Migrate your code now</strong></a> or <a href="https://www.getbowtied.com/documentation/shopkeeper/important-notice/" target="_blank"><strong>Read more</strong></a>.
+			</p>
+		</div>
+		<?php
+	}
+
+	return;
+}
+add_action( 'admin_notices', 'shopkeeper_extender_notifications' );
+
+
+// Admin dismiss notices
+
+function shopkeeper_extender_notifications_dismiss() {
+	if( $_POST['notice'] == 'remove-additional-js-notice' ) {
+		update_option('dismissed-sk-ext-notification-remove-additional-js', TRUE );
+	}
+}
+add_action( 'wp_ajax_sk_ext_notifications_dismiss', 'shopkeeper_extender_notifications_dismiss' );
--- includes/helpers/js/other_plugins.js
+++ includes/helpers/js/other_plugins.js
@@ -2,6 +2,16 @@ jQuery(function($) {
 
 	"use strict";
 
+	// dismiss dashboard notices
+	$( document ).on( 'click', '.remove-additional-js-notice .notice-dismiss', function () {
+		var data = {
+            'action' : 'sk_ext_notifications_dismiss',
+            'notice' : 'remove-additional-js-notice'
+        };
+
+        jQuery.post( 'admin-ajax.php', data );
+	});
+
 	//Custom CSS and JS
 	$(".ccj__notice a:not(.dismiss_notice), .ccj_only_premium a")
 		.attr("href", "https://getbowtied.net/simple-custom-css-js-plugin");
